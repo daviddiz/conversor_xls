c9bcec35e39ba750283d6a3d12c6972e4d4e3905
daviddiz <daviddiz@gmail.com>
2015-05-11 01:09:26 +0200

    ejemplo completo

--- conversor_xls.py
+++ conversor_xls.py
@@ -1,14 +1,67 @@
-#    para lanzar script de ejemplo ejecutar
-#    python /home/david/Escritorio/xlrd-0.9.3/scripts/runxlrd.py 3rows *blah*.xls
-
+from Tkinter import *
+from tkMessageBox import *
+from FileDialog import *
 import xlrd
-book = xlrd.open_workbook("/home/david/Escritorio/openinnova/desglose nomina.xls")
-print "The number of worksheets is", book.nsheets
-print "Worksheet name(s):", book.sheet_names()
-sh = book.sheet_by_index(0)
-print sh.name, sh.nrows, sh.ncols
-print "Cell D30 is", sh.cell_value(rowx=29, colx=3)
-for rx in range(sh.nrows):
-    print sh.row(rx)
-# Refer to docs for more details.
-# Feedback on API is welcomed.
+
+def test():
+    """Simple test program."""
+    root = Tk()
+    root.withdraw()
+    fd = LoadFileDialog(root)
+    loadfile = fd.go(key="test")
+    loadfile_name = loadfile.split("/")[-1][:-4]
+    new_file = loadfile_name + "_convertido.txt"
+    
+    book = xlrd.open_workbook(loadfile)
+    sh = book.sheet_by_index(0)
+    
+    with open(new_file, "w") as f:
+        f.write(sh.cell_value(rowx=16, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=3))
+        f.write("\n")
+        
+        f.write(sh.cell_value(rowx=16, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=5))
+        f.write("\n")
+        
+        f.write(sh.cell_value(rowx=16, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=7))
+        f.write("\n")
+    f.close()
+
+
+if __name__ == '__main__':
+    test()
--- tkinter_example.py
+++ tkinter_example.py
@@ -1,6 +1,7 @@
 from Tkinter import *
 from tkMessageBox import *
 from FileDialog import *
+import xlrd
 
 # def answer():
 #     showerror("Answer", "Sorry, no answer available")
@@ -22,9 +23,72 @@ def test():
     root.withdraw()
     fd = LoadFileDialog(root)
     loadfile = fd.go(key="test")
-    fd = SaveFileDialog(root)
-    savefile = fd.go(key="test")
-    print loadfile, savefile
+    loadfile_name = loadfile.split("/")[-1][:-4]
+    new_file = loadfile_name + "_convertido.txt"
+    
+#     print loadfile
+    
+    book = xlrd.open_workbook(loadfile)
+    sh = book.sheet_by_index(0)
+    
+    with open(new_file, "w") as f:
+        f.write(sh.cell_value(rowx=16, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=3))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=3))
+        f.write("\n")
+        
+        f.write(sh.cell_value(rowx=16, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=5))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=5))
+        f.write("\n")
+        
+        f.write(sh.cell_value(rowx=16, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=9, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=28, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=14, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=22, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=21, colx=7))
+        f.write("\t")
+        f.write(sh.cell_value(rowx=8, colx=7))
+        f.write("\n")
+    f.close()
+    
+#     print "The number of worksheets is", book.nsheets
+#     print "Worksheet name(s):", book.sheet_names()
+#     sh = book.sheet_by_index(0)
+#     print sh.name, sh.nrows, sh.ncols
+#     print "Cell D30 is", sh.cell_value(rowx=29, colx=3)
+#     for rx in range(sh.nrows):
+#         print sh.row(rx)
+    
+#     fd = SaveFileDialog(root)
+#     savefile = fd.go(key="test")
+#     print loadfile, savefile
 
 
 if __name__ == '__main__':
