a026f13865733973e22e84a51fc82510a4a9975f
daviddiz <daviddiz@gmail.com>
2015-05-11 15:23:02 +0200

    parse to string

---
+++ setup.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+
+from distutils.core import setup 
+import py2exe
+import xml.etree.ElementTree
+ 
+setup(name="Conversor", 
+ version="1.0", 
+ description="Conversor de xls a txt", 
+ author="David Diz", 
+ author_email="daviddiz@gmail.com", 
+ scripts=["conversor_xls.py"], 
+ console=["conversor_xls.py"], 
+ options={"py2exe": 		{"packages":["encodings"],
+                 "includes":["xlrd","datetime", "decimal"],
+                 "bundle_files":2,
+                 "optimize":2},}, 
+ zipfile=None,
+)
